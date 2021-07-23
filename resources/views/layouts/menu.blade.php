b04c4ea7cc6b5d2a41d55476e2f3960b143eb570
Ademir Silva <ademir.silva@aluno.ifsertao-pe.edu.br>
2021-07-23 00:47:42 -0300

    ADM | Atualização de cores, ajustes no layout....

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,7 +4,7 @@
 
 <nav class="navbar">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
+      <a class="navbar-brand" href="{{ url('/painel') }}">
         <img src="images/Logo 1.png" alt="" width="100px" class="d-inline-block align-text-top">
       </a>
     </div>
